e4bfee27172f066d6ebe92bdcfc1254dde5a40af
ZicoG <zgatsjad8897@student.landstede.nl>
2017-09-13 09:24:56 +0200

    saaf slaaf

--- functions/functionsCMS.php
+++ functions/functionsCMS.php
@@ -42,6 +42,29 @@ function footer() {
     <script src="/rental-oefen-examen/rental-car-project/js/jquery-3.2.1.min.js"></script>
     <script src="/rental-oefen-examen/rental-car-project/js/bootstrap.min4.js"></script>
 
+    <script>
+        $(function () {
+            $('.navbar-toggle').click(function () {
+                $('.navbar-nav').toggleClass('slide-in');
+                $('.side-body').toggleClass('body-slide-in');
+                $('#search').removeClass('in').addClass('collapse').slideUp(200);
+
+                /// uncomment code for absolute positioning tweek see top comment in css
+                //$('.absolute-wrapper').toggleClass('slide-in');
+
+            });
+
+            // Remove menu for searching
+            $('#search-trigger').click(function () {
+                $('.navbar-nav').removeClass('slide-in');
+                $('.side-body').removeClass('body-slide-in');
+
+                /// uncomment code for absolute positioning tweek see top comment in css
+                //$('.absolute-wrapper').removeClass('slide-in');
+
+            });
+        });
+    </script>
 
 
     </body>
